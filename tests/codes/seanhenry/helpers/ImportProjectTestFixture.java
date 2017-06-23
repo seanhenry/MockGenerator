ae79201e27af7d494761c7da254dccb031bcfb39
Sean Henry <hello@seanhenry.codes>
2017-06-23 12:42:08 -0400

    Adds less hacky solution for writing tests

---
+++ tests/codes/seanhenry/helpers/ImportProjectTestFixture.java
@@ -0,0 +1,87 @@
+package codes.seanhenry.helpers;
+
+import com.intellij.ide.impl.ProjectUtil;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ex.ProjectManagerEx;
+import com.intellij.testFramework.fixtures.IdeaProjectTestFixture;
+import com.intellij.testFramework.fixtures.TempDirTestFixture;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+public class ImportProjectTestFixture implements IdeaProjectTestFixture {
+
+  private final String projectDirectoryPath;
+  private final String projectFileName;
+  private final TempDirTestFixture tempDirTestFixture;
+  private Project project;
+
+  public ImportProjectTestFixture(String projectDirectoryPath, String projectFileName, TempDirTestFixture tempDirTestFixture) {
+    this.projectFileName = projectFileName;
+    this.tempDirTestFixture = tempDirTestFixture;
+    this.projectDirectoryPath = projectDirectoryPath;
+  }
+
+  @Override
+  public void setUp() throws Exception {
+    String tempDirectory = tempDirTestFixture.getTempDirPath();
+    copyFolder(new File(projectDirectoryPath), new File(tempDirectory));
+    project = ProjectUtil.openOrImport(tempDirectory + "/" + projectFileName, null, true);
+    ProjectManagerEx.getInstanceEx().openTestProject(getProject());
+  }
+
+  @Override
+  public void tearDown() throws Exception {
+    if (getProject() != null) {
+      ProjectManagerEx.getInstanceEx().closeTestProject(getProject());
+      ProjectUtil.closeAndDispose(getProject());
+      project = null;
+    }
+  }
+
+  @Override
+  public Project getProject() {
+    return project;
+  }
+
+  @Override
+  public Module getModule() {
+    return null;
+  }
+
+  private void copyFolder(File sourceFolder, File destinationFolder) throws IOException {
+    //Check if sourceFolder is a directory or file
+    //If sourceFolder is file; then copy the file directly to new location
+    if (sourceFolder.isDirectory())
+    {
+      //Verify if destinationFolder is already present; If not then create it
+      if (!destinationFolder.exists())
+      {
+        destinationFolder.mkdir();
+        //System.out.println("Directory created :: " + destinationFolder);
+      }
+
+      //Get all files from source directory
+      String files[] = sourceFolder.list();
+
+      //Iterate over all files and copy them to destinationFolder one by one
+      for (String file : files)
+      {
+        File srcFile = new File(sourceFolder, file);
+        File destFile = new File(destinationFolder, file);
+
+        //Recursive function call
+        copyFolder(srcFile, destFile);
+      }
+    }
+    else
+    {
+      //Copy the file content from one place to another
+      Files.copy(sourceFolder.toPath(), destinationFolder.toPath(), StandardCopyOption.REPLACE_EXISTING);
+      //System.out.println("File copied :: " + destinationFolder);
+    }
+  }
+}
--- tests/codes/seanhenry/intentions/MockGeneratingIntentionTest.java
+++ tests/codes/seanhenry/intentions/MockGeneratingIntentionTest.java
@@ -6,22 +6,18 @@ import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess;
 import com.intellij.psi.*;
 import com.intellij.psi.search.FilenameIndex;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.testFramework.*;
 import com.intellij.testFramework.fixtures.*;
-import com.intellij.testFramework.fixtures.impl.CodeInsightTestFixtureImpl;
 import com.intellij.testFramework.fixtures.impl.TempDirTestFixtureImpl;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
+import java.io.IOException;
 
-public class MockGeneratingIntentionTests extends PlatformTestCase {
+public class MockGeneratingIntentionTest extends PlatformTestCase {
 
-  private final String dataPath = "/Users/sean/source/plugins/community/MockGenerator/testData/TestProject";
+  private static final String dataPath = "/Users/sean/source/plugins/community/MockGenerator/testData/TestProject";
   private CodeInsightTestFixture myFixture;
 
   @Override
@@ -32,7 +28,6 @@ public class MockGeneratingIntentionTests extends PlatformTestCase {
     } finally {
       myFixture = null;
       try {
-
         super.tearDown();
       } catch (Throwable ignored) {}
     }
@@ -42,15 +37,8 @@ public class MockGeneratingIntentionTests extends PlatformTestCase {
   protected void setUpProject() throws Exception {
     super.setUpProject();
 
-    VfsRootAccess.allowRootAccess("/Applications/Xcode-8.3.2.app/Contents");
-
     TempDirTestFixtureImpl tempDirTestFixture = new TempDirTestFixtureImpl();
-    String projectDir = tempDirTestFixture.getTempDirPath();
-    copyFolder(new File(dataPath), new File(projectDir));
-
-    String name = getClass().getName() + "." + getName();
-    myFixture = new CodeInsightTestFixtureImpl(new MyHeavyIdeaTestFixtureImpl(name, projectDir + "/TestProject.xcodeproj"), tempDirTestFixture);
-
+    myFixture = IdeaTestFixtureFactory.getFixtureFactory().createCodeInsightFixture(new ImportProjectTestFixture(dataPath, "TestProject.xcodeproj", tempDirTestFixture), tempDirTestFixture);
     myFixture.setUp();
     myFixture.setTestDataPath(dataPath);
   }
@@ -94,38 +82,5 @@ public class MockGeneratingIntentionTests extends PlatformTestCase {
 
     WriteCommandAction.runWriteCommandAction(getActiveProject(), () -> action.invoke(getActiveProject(), myFixture.getEditor(), psiFile));
     myFixture.checkResultByFile(expectedFileName, true);
-  }
-
-  private void copyFolder(File sourceFolder, File destinationFolder) throws IOException {
-    //Check if sourceFolder is a directory or file
-    //If sourceFolder is file; then copy the file directly to new location
-    if (sourceFolder.isDirectory())
-    {
-      //Verify if destinationFolder is already present; If not then create it
-      if (!destinationFolder.exists())
-      {
-        destinationFolder.mkdir();
-        //System.out.println("Directory created :: " + destinationFolder);
-      }
-
-      //Get all files from source directory
-      String files[] = sourceFolder.list();
-
-      //Iterate over all files and copy them to destinationFolder one by one
-      for (String file : files)
-      {
-        File srcFile = new File(sourceFolder, file);
-        File destFile = new File(destinationFolder, file);
-
-        //Recursive function call
-        copyFolder(srcFile, destFile);
-      }
-    }
-    else
-    {
-      //Copy the file content from one place to another
-      Files.copy(sourceFolder.toPath(), destinationFolder.toPath(), StandardCopyOption.REPLACE_EXISTING);
-      //System.out.println("File copied :: " + destinationFolder);
-    }
   }
 }
