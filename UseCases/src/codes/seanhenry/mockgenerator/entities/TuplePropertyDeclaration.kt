b41e26eeed67da08125079858f1823ae34cc3e9f
Sean Henry <hello@seanhenry.codes>
2017-09-17 16:07:03 -0500

    Refactors global method to companion object

--- UseCases/src/codes/seanhenry/mockgenerator/entities/TuplePropertyDeclaration.kt
+++ UseCases/src/codes/seanhenry/mockgenerator/entities/TuplePropertyDeclaration.kt
@@ -2,16 +2,19 @@ package codes.seanhenry.mockgenerator.entities
 
 class TuplePropertyDeclaration(name: String, val parameters: List<TupleParameter>): PropertyDeclaration(name, createType(parameters)) {
   class TupleParameter(val name: String, val type: String)
-}
 
-private fun createType(parameters: List<TuplePropertyDeclaration.TupleParameter>) =
-    "(" + parameters
-        .map { buildParameterString(it) }
-        .joinToString(", ") + ")"
+  companion object {
+    private fun createType(parameters: List<TuplePropertyDeclaration.TupleParameter>) =
+        "(" + parameters
+            .map { buildParameterString(it) }
+            .joinToString(", ") + ")"
 
-private fun buildParameterString(it: TuplePropertyDeclaration.TupleParameter): String {
-  if (it.name.isEmpty()) {
-    return it.type
+    private fun buildParameterString(it: TuplePropertyDeclaration.TupleParameter): String {
+      if (it.name.isEmpty()) {
+        return it.type
+      }
+      return it.name + ": " + it.type
+    }
   }
-  return it.name + ": " + it.type
 }
+
