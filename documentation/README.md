c3bda3e3aca0b689e52fbee974cf3407b7e4a17f
Sean Henry <hello@seanhenry.codes>
2018-01-17 14:47:29 -0800

    Adds groups to table of contents

--- documentation/README.md
+++ documentation/README.md
@@ -1,3 +1,5 @@
 # Table of Contents
+## 1 - Query
 - [Was a method invoked?](invokeMethod.md)
 - [How many times was a method invoked?](invokeMethodCount.md)
+## 2 - Stub
--- ruby/tableofcontents.rb
+++ ruby/tableofcontents.rb
@@ -1,17 +1,24 @@
 require 'mustache'
 require 'json'
 
-contents = []
-Dir.glob('examples/*.json') do |file|
-  contents.push(
-      title: JSON.parse(File.read(file))['title'],
-      link: file.sub('examples/', '').sub('.json', '.md')
-  )
+def get_items(dir)
+  Dir.glob("#{dir}*.json").map do |file|
+    {
+        title: JSON.parse(File.read(file))['title'],
+        link: file.sub(dir, '').sub('.json', '.md')
+    }
+  end
 end
+
+groups = Dir.foreach('examples')
+             .sort
+             .reject { |f| f == '.' || f == '..' }
+             .select { |f| File.directory?("examples/#{f}") }
+             .map { |dir| { title: dir, items: get_items("examples/#{dir}/") } }
 
 class TableOfContents < Mustache
 end
 
 template = TableOfContents.new
-template[:item] = contents
+template[:groups] = groups
 File.write('../documentation/README.md', template.render)
