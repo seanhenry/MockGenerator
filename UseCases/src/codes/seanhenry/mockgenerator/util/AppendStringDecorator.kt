c6f127daa112334aadc1c33d4d44574cadc70428
Sean Henry <hello@seanhenry.codes>
2018-01-18 15:50:19 -0800

    Fixes warnings

--- UseCases/src/codes/seanhenry/mockgenerator/util/AppendStringDecorator.kt
+++ UseCases/src/codes/seanhenry/mockgenerator/util/AppendStringDecorator.kt
@@ -3,7 +3,7 @@ package codes.seanhenry.mockgenerator.util
 class AppendStringDecorator(decorator: StringDecorator?, private val suffix: String) : StringDecorator(decorator) {
 
   public override fun decorate(string: String): String {
-    if (string == null || string.isEmpty()) return ""
-    return if (suffix == null || suffix.isEmpty()) string else string + suffix
+    if (string.isEmpty()) return ""
+    return if (suffix.isEmpty()) string else string + suffix
   }
 }
--- UseCases/src/codes/seanhenry/mockgenerator/util/PrependStringDecorator.kt
+++ UseCases/src/codes/seanhenry/mockgenerator/util/PrependStringDecorator.kt
@@ -3,8 +3,8 @@ package codes.seanhenry.mockgenerator.util
 class PrependStringDecorator(decorator: StringDecorator?, private val prefix: String) : StringDecorator(decorator) {
 
   override fun decorate(string: String): String {
-    if (string == null || string.isEmpty()) return ""
-    if (prefix == null || prefix.isEmpty()) return string
+    if (string.isEmpty()) return ""
+    if (prefix.isEmpty()) return string
     var capitalised = string.substring(0, 1).toUpperCase()
     if (1 < string.length)
       capitalised += string.substring(1)
