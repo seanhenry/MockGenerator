7edb361ca9cfc7314ce5d3208e5dfd206d6ec760
Sean Henry <hello@seanhenry.codes>
2018-04-09 18:41:19 +1000

    Adds builder for property

--- UseCases/src/main/java/codes/seanhenry/mockgenerator/entities/Property.kt
+++ UseCases/src/main/java/codes/seanhenry/mockgenerator/entities/Property.kt
@@ -1,11 +1,38 @@
 package codes.seanhenry.mockgenerator.entities
 
-class Property(val name: String, val type: TypeIdentifier, val isWritable: Boolean, val declarationText: String) {
+class Property(val name: String, val type: Type, val isWritable: Boolean, val declarationText: String) {
 
   // TODO: remove and make builder
-  constructor(name: String, type: String, isWritable: Boolean, declarationText: String): this(name, TypeIdentifier(type), isWritable, declarationText)
+  constructor(name: String,
+              type: String,
+              isWritable: Boolean,
+              declarationText: String) : this(name, TypeIdentifier(type), isWritable, declarationText)
 
   fun getTrimmedDeclarationText(): String {
     return declarationText.split("{")[0].trimEnd(' ', '\t', '\n')
+  }
+
+  class Builder(private val name: String) {
+
+    private var type: Type = TypeIdentifier.EMPTY
+    private var isWritable = true
+
+    fun let(): Builder {
+      isWritable = false
+      return this
+    }
+
+    fun type(identifier: String): Builder {
+      type = TypeIdentifier(identifier)
+      return this
+    }
+
+    fun type(): TypeIdentifier.Factory<Builder> {
+      return TypeIdentifier.Factory(this) { this.type = it }
+    }
+
+    fun build(): Property {
+      return Property(name, type, isWritable, "var $name: ${type.text}")
+    }
   }
 }
